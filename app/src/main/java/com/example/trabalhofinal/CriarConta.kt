1095dc023875877f88291fc327024a0b41237d6f
diogop2002 <aluno23030@ipt.pt>
2023-12-29 22:19:09 +0000

    Sistema de verificaçao de usernames repetidos

--- app/src/main/java/com/example/trabalhofinal/CriarConta.kt
+++ app/src/main/java/com/example/trabalhofinal/CriarConta.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trabalhofinal.model.User
 import com.example.trabalhofinal.model.UserRequest
+import com.example.trabalhofinal.model.UserResponse
 import com.example.trabalhofinal.retrofit.RetrofitInitializer
 import retrofit2.Call
 import retrofit2.Callback
@@ -30,63 +31,91 @@ class CriarConta : AppCompatActivity() {
     }
 
     private fun processarCriarConta() {
-        // Obter referências para os EditTexts
         val editTextNome = findViewById<EditText>(R.id.editTextNome)
         val editTextApelido = findViewById<EditText>(R.id.editTextApelido)
         val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
         val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
         val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
 
-        // Obter os valores inseridos pelos usuários
         val nome = editTextNome.text.toString()
         val apelido = editTextApelido.text.toString()
         val username = editTextUsername.text.toString()
         val email = editTextEmail.text.toString()
         val password = editTextPassword.text.toString()
 
-        // Verificar se todos os campos estão preenchidos
         if (nome.isNotEmpty() && apelido.isNotEmpty() && username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
-            // Verificar o formato do e-mail
             if (validaEmail(email)) {
-                // Continuar com o processamento
                 val utilizador = User(null, nome, apelido, username, email, password)
                 val user = UserRequest(utilizador)
-                val call = RetrofitInitializer().service().addUser("Bearer Tostas",user)
 
-                call.enqueue(object : Callback<UserRequest?> {
-                    override fun onResponse(call: Call<UserRequest?>, response: Response<UserRequest?>) {
-                        if (response.isSuccessful) {
-                            // A requisição foi bem-sucedida, processar a resposta se necessário
-                            val userResponse = response.body()
-                            if (response.code() == 200) {
-                                if (userResponse != null) {
-                                    Log.e("Novo usuário adicionado", userResponse.user.toString())
-                                }
-                            }
-                        } else {
-                            // A requisição falhou, verificar o código de resposta e tratar conforme necessário
-                            Log.e("Falha na adição de usuário", "Código de resposta: ${response.code()}")
-                        }
-                    }
-
-                    override fun onFailure(call: Call<UserRequest?>, t: Throwable) {
-                        // Ocorreu uma falha na chamada à API, tratar conforme necessário
-                        t.printStackTrace()
-                        t.message?.let { Log.e("Falha na Chamada à API", it) }
-                    }
-                })
-
-                Toast.makeText(this, "Conta criada com sucesso!", Toast.LENGTH_SHORT).show()
-                abrirMain()
+                // Verifica se o username já existe antes de tentar criar o usuário
+                getUsersApi(username, user)
             } else {
-                // Exiba uma mensagem de erro informando que o formato do e-mail é inválido
                 Toast.makeText(this, "Formato de e-mail inválido", Toast.LENGTH_SHORT).show()
             }
         } else {
-            // Se algum campo estiver vazio, exibir uma mensagem de erro
             Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
         }
     }
+
+    private fun getUsersApi(username: String, user: UserRequest) {
+        val call = RetrofitInitializer().service().getUsers("Bearer Tostas")
+        call.enqueue(object : Callback<UserResponse> {
+            override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
+                if (response.isSuccessful) {
+                    val users = response.body()?.users
+                    if (users != null) {
+                        val matchedUser = users.find { it.username == username }
+                        if (matchedUser != null) {
+                            // Usuário já existe, exibe uma mensagem de erro
+                            Toast.makeText(this@CriarConta, "Username já existente. Insira outro username", Toast.LENGTH_SHORT).show()
+                        } else {
+                            // Usuário não existe, prossegue com o registro
+                            realizarRegisto(user)
+                        }
+                    } else {
+                        Log.e("Erro", "Lista de usuários nula.")
+                    }
+                } else {
+                    Log.e("Erro", "Erro na chamada à API: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                t.printStackTrace()
+                Log.e("Erro", "Erro na chamada à API: ${t.message}")
+            }
+        })
+    }
+
+    private fun realizarRegisto(user: UserRequest) {
+        val call = RetrofitInitializer().service().addUser("Bearer Tostas", user)
+        call.enqueue(object : Callback<UserRequest?> {
+            override fun onResponse(call: Call<UserRequest?>, response: Response<UserRequest?>) {
+                if (response.isSuccessful) {
+                    val userResponse = response.body()
+                    if (response.code() == 200) {
+                        if (userResponse != null) {
+                            Log.e("Novo usuário adicionado", userResponse.user.toString())
+                            Toast.makeText(this@CriarConta, "Conta criada com sucesso!", Toast.LENGTH_SHORT).show()
+                            abrirMain()
+                        }
+                    }
+                } else {
+                    Log.e("Falha na adição de usuário", "Código de resposta: ${response.code()}")
+                    Toast.makeText(this@CriarConta, "Falha ao criar conta. Tente novamente.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<UserRequest?>, t: Throwable) {
+                t.printStackTrace()
+                Log.e("Falha na Chamada à API", t.message ?: "Erro desconhecido")
+                Toast.makeText(this@CriarConta, "Falha ao criar conta. Tente novamente.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+
 
     // Função que faz com que vá para o menu inicial da app (main)
     private fun abrirMain() {
