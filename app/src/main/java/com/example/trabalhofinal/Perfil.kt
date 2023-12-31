b5fb6b3ebfb03f5548e57d179947ca4a37596b24
aluno22116 <aluno22116@ipt.pt>
2023-12-31 00:23:42 +0000

    acesso á camera na view perfil quando se clica no botão

--- app/src/main/java/com/example/trabalhofinal/Perfil.kt
+++ app/src/main/java/com/example/trabalhofinal/Perfil.kt
@@ -1,11 +1,32 @@
 package com.example.trabalhofinal
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.example.trabalhofinal.databinding.ActivityPerfilBinding
 import com.google.android.material.navigation.NavigationView
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class Perfil : TesteMenu() {
+
     private lateinit var binding: ActivityPerfilBinding
+    private val ourRequestCode = 123
+    private val cameraPermissionRequest = 124
+    private lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,10 +34,90 @@ class Perfil : TesteMenu() {
         binding = ActivityPerfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Se necessário, configure o NavigationView da classe Perfil
+        // Configurar o NavigationView da classe Perfil
         val navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener { menuItem ->
             handleMenuItemClick(menuItem.itemId)
+            true
         }
     }
+
+    fun takePhoto(view: View) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                cameraPermissionRequest
+            )
+        } else {
+            iniciarCamera()
+        }
+    }
+
+    private fun iniciarCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            val photoFile: File? = try {
+                criarArquivoImagem()
+            } catch (ex: IOException) {
+                null
+            }
+
+            photoFile?.also {
+                val photoURI: Uri = FileProvider.getUriForFile(
+                    this,
+                    "com.example.trabalhofinal.fileprovider",
+                    it
+                )
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+
+                startActivityForResult(intent, ourRequestCode)
+            }
+        }
+    }
+
+    private fun criarArquivoImagem(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == ourRequestCode && resultCode == RESULT_OK) {
+            val imageView: ImageView = findViewById(R.id.fotografia)
+            val bitmap = data?.extras?.get("data") as? Bitmap
+            bitmap?.let {
+                imageView.setImageBitmap(it)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == cameraPermissionRequest && grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            iniciarCamera()
+        }
+    }
+
+
 }
