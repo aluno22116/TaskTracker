1a58452b77efd349c2aeb3944f5983eda072f97e
aluno22116 <aluno22116@ipt.pt>
2024-01-24 16:49:49 +0000

    Correção de erros no menu principal, criação da view bloco de notas e da criar notas

--- app/src/main/java/com/example/trabalhofinal/Menuprincipal.kt
+++ app/src/main/java/com/example/trabalhofinal/Menuprincipal.kt
@@ -1,9 +1,8 @@
 package com.example.trabalhofinal
 
-
+import BlocoNotas
 import Perfil
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import com.example.trabalhofinal.databinding.ActivityMenuprincipalBinding
 import com.google.android.material.navigation.NavigationView
@@ -19,7 +18,7 @@ class Menuprincipal : TesteMenu() {
         setContentView(binding.root)
 
         setupButtonListeners()
-
+        //abrirDrawerBtn()
         // Configurar o NavigationView
         val navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener { menuItem ->
@@ -30,7 +29,7 @@ class Menuprincipal : TesteMenu() {
     private fun setupButtonListeners() {
         val buttonCriadores = findViewById<Button>(R.id.vamosver)
         val buttonPerfil = findViewById<Button>(R.id.perfil)
-        //val buttonBNotas = findViewById<Button>(R.id.buttonBNotas)
+        val buttonBNotas = findViewById<Button>(R.id.notas)
 
         buttonCriadores.setOnClickListener {
             //it.startAnimation(AnimationUtils.loadAnimation(this, R.anim.button_click_animation))
@@ -41,10 +40,10 @@ class Menuprincipal : TesteMenu() {
             //it.startAnimation(AnimationUtils.loadAnimation(this, R.anim.button_click_animation))
             abrirPerfil(Perfil())
         }
-       /* buttonBNotas.setOnClickListener {
-            it.startAnimation(AnimationUtils.loadAnimation(this, R.anim.button_click_animation))
-            abrirBnotas(Bnotas())
-        }*/
+        buttonBNotas.setOnClickListener {
+          //  it.startAnimation(AnimationUtils.loadAnimation(this, R.anim.button_click_animation))
+            abrirBnotas(BlocoNotas())
+        }
     }
 
     private fun abrirCriadores(fragment: vamosver) {
@@ -61,15 +60,27 @@ class Menuprincipal : TesteMenu() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, balanceViewFragment)
             .commit()
-        Log.e("ERRO","CLICASTE NO CARALHO DO BOTÃO")
     }
 
 
-   /* private fun abrirBnotas(fragment: Bnotas) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragmento, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
+    private fun abrirBnotas(fragment: BlocoNotas) {
+        val balanceViewFragment = BlocoNotas()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, balanceViewFragment)
+            .commit()
+    }
+
+   /* private fun abrirDrawerBtn(){
+    val btnMenuDrawer = findViewById<ImageButton>(R.id.btnMenuDrawer)
+        btnMenuDrawer.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START,true)
+        }
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
     }*/
 
 }
--- app/src/main/java/com/example/trabalhofinal/retrofit/RetrofitInitializer.kt
+++ app/src/main/java/com/example/trabalhofinal/retrofit/RetrofitInitializer.kt
@@ -1,14 +1,14 @@
 package com.example.trabalhofinal.retrofit
 
+import com.example.trabalhofinal.retrofit.service.Service
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.example.trabalhofinal.retrofit.service.Service
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInitializer {
     private val gson: Gson = GsonBuilder().setLenient().create()
-    private val host = "https://api.sheety.co/fbdf7989b3d662d677c247d1ef56dc15/dam/"
+    private val host = "https://api.sheety.co/8985fe49fbeb8d6428ce3ca585ec2675/dam/"
 
 
 
