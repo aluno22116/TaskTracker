1fae005e5c2e6dbe3a08b77c3ab8e9de77b6a286
diogop2002 <aluno23030@ipt.pt>
2024-01-25 17:11:14 +0000

    correçao de criar notas para cada utilizador

--- app/src/main/java/com/example/trabalhofinal/CriarNotas.kt
+++ app/src/main/java/com/example/trabalhofinal/CriarNotas.kt
@@ -13,6 +13,7 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.trabalhofinal.model.Note
 import com.example.trabalhofinal.model.NoteRequest
+import com.example.trabalhofinal.model.NoteResponse
 import com.example.trabalhofinal.retrofit.RetrofitInitializer
 import retrofit2.Call
 import retrofit2.Callback
@@ -70,12 +71,9 @@ class CriarNotas : Fragment() {
                 if (response.isSuccessful) {
                     Log.i("ALERTA!!!!!!!!", "Nota colocada na API")
 
-                    // Substituir ou adicionar o fragmento BlocoNotas
-                    val blocoNotasFragment = BlocoNotas()
-                    val transaction = requireActivity().supportFragmentManager.beginTransaction()
-                    transaction.replace(R.id.fragment_container, blocoNotasFragment)
-                    transaction.addToBackStack(null)
-                    transaction.commit()
+
+                    getNotes(userId)
+
                 } else {
                     Log.e("Erro", "Erro na chamada à API de atualização de nota: ${response.message()}")
                 }
@@ -87,6 +85,86 @@ class CriarNotas : Fragment() {
             }
         })
     }
+
+    private fun getNotes(userId: String) {
+        // val userId = getSavedUserId()
+
+        // Verifica se o userId é válido
+        if (userId.isNotEmpty()) {
+            val call = RetrofitInitializer().service().getNotes("Bearer Tostas", userId)
+
+            call.enqueue(object : Callback<NoteResponse> {
+                override fun onResponse(call: Call<NoteResponse>, response: Response<NoteResponse>) {
+                    if (response.isSuccessful) {
+                        val noteResponse = response.body()
+                        if (noteResponse != null) {
+                            val notes = noteResponse.notes
+                            val notasStrings = mutableListOf<String>()
+
+
+                            if (notes != null && notes.isNotEmpty()) {
+                                // O usuário tem notas, faça algo com as notas obtidas da API
+                                // Por exemplo, exibir ou processar as notas
+                                Log.e("Notas", "O usuário tem notas")
+                                Log.i("INFO", "Usuário encontrado: $notes")
+
+                                // Itera sobre a lista de notas e obtém o parâmetro 'notas'
+                                for (note in notes) {
+                                    // Verifica se o ID do usuário da nota é o mesmo que o usuário logado
+                                    if (note.id.toString() == userId) {
+                                        val nota = note.notas
+                                        if (nota != null) {
+                                            notasStrings.add(nota)
+                                        }
+                                    }
+                                }
+
+
+                                // Converte a lista de strings em uma string separada por algum caractere, por exemplo, vírgula
+                                val notesString = notasStrings.joinToString()
+
+                                // Salva a string na SharedPreferences
+                                saveNotesToSharedPreferences(notesString)
+
+                                // Substituir ou adicionar o fragmento BlocoNotas
+                                val blocoNotasFragment = BlocoNotas()
+                                val transaction = requireActivity().supportFragmentManager.beginTransaction()
+                                transaction.replace(R.id.fragment_container, blocoNotasFragment)
+                                transaction.addToBackStack(null)
+                                transaction.commit()
+                            } else {
+                                // O usuário não tem notas
+                                Log.e("Notas", "O usuário não tem notas")
+                                // Aqui você pode decidir o que fazer quando o usuário não tem notas
+                                // Por exemplo, exibir uma mensagem ou realizar alguma outra ação
+                            }
+                        } else {
+                            Log.e("Erro", "Resposta nula.")
+                        }
+                    } else {
+                        Log.e("Erro", "Erro na chamada à API: ${response.message()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<NoteResponse>, t: Throwable) {
+                    t.printStackTrace()
+                    Log.e("Erro", "Erro na chamada à API: ${t.message}")
+                }
+            })
+        } else {
+            // userId não é válido, faça algo aqui se necessário
+            Log.e("Erro", "userId inválido")
+        }
+    }
+
+    // Função para salvar a string na SharedPreferences
+    private fun saveNotesToSharedPreferences(notesString: String) {
+        val sharedPreferences =  requireActivity().getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("notes", notesString)
+        editor.apply()
+    }
+
 
     private fun notesToString(notes: List<Note>): String {
         return notes.joinToString { it.toString() }
